5474b4ad400dfe0335bdc277d39f4590c576851b
Michael Drogalis <mjd3089@rit.edu>
2013-09-20 10:40:18 -0400

    project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject night-vision "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "See through the darkness of a running program"
+  :url "https://github.com/MichaelDrogalis/night-vision"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
