4a9ce38ec2ed64b63d1aafdf284d02a7b53c5419
Michael Drogalis <mjd3089@rit.edu>
2013-09-20 11:19:27 -0400

    ns

--- src/night_vision/goggles.clj
+++ src/night_vision/goggles.clj
@@ -1,22 +1,22 @@
 (ns night-vision.goggles
-  (:require [clojure.pprint]
-            [dire.core]))
+  (:require [clojure.pprint :refer [pprint]]
+            [dire.core :as d]))
 
 (defn introspect-ns! [target-ns]
   (let [all-vars (vals (ns-publics target-ns))
         fns (filter #(contains? (meta %) :arglists) all-vars)
         divider "------------------------------------------------"]
     (doseq [f fns]
-      (dire.core/with-pre-hook! f
+      (d/with-pre-hook! f
         (fn [& args]
-          (clojure.pprint/pprint divider)
-          (clojure.pprint/pprint (str "Invoking " f))
-          (clojure.pprint/pprint (str "Actual arguments are: " args))
-          (clojure.pprint/pprint divider)))
-      (dire.core/with-post-hook! f
+          (pprint divider)
+          (pprint (str "Invoking " f))
+          (pprint (str "Actual arguments are: " args))
+          (pprint divider)))
+      (d/with-post-hook! f
         (fn [result]
-          (clojure.pprint/pprint divider)
-          (clojure.pprint/pprint (str "Returning from " f))
-          (clojure.pprint/pprint (str "Return value is: " result))
-          (clojure.pprint/pprint divider))))))
+          (pprint divider)
+          (pprint (str "Returning from " f))
+          (pprint (str "Return value is: " result))
+          (pprint divider))))))
 
