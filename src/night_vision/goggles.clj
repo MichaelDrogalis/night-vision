9a5802dcc2e73c3a29d5679070aff5b98102991b
Michael Drogalis <mjd3089@rit.edu>
2013-11-12 11:09:02 -0500

    Pull line out.

--- src/night_vision/goggles.clj
+++ src/night_vision/goggles.clj
@@ -2,29 +2,31 @@
   (:require [clojure.pprint :refer [pprint]]
             [dire.core :as d]))
 
-(defn pre-log [f divider]
-  (d/with-pre-hook! f
-    (fn [& args]
-      (pprint divider)
-      (pprint (str "Invoking " f))
-      (pprint (str "Actual arguments are: " args))
-      (pprint divider))))
+(defonce pre-log
+  (fn [f divider]
+    (d/with-pre-hook! f
+      (fn [& args]
+        (pprint divider)
+        (pprint (str "Invoking " f))
+        (pprint (str "Actual arguments are: " args))
+        (pprint divider)))))
 
-(defn post-log [f divider]
-  (d/with-post-hook! f
-    (fn [result]
-      (pprint divider)
-      (pprint (str "Returning from " f))
-      (pprint (str "Return value is: " result))
-      (pprint divider))))
+(defonce post-log
+  (fn [f divider]
+    (d/with-post-hook! f
+      (fn [result]
+        (pprint divider)
+        (pprint (str "Returning from " f))
+        (pprint (str "Return value is: " result))
+        (pprint divider)))))
 
 (defn introspect-ns! [target-ns]
   (let [all-vars (vals (ns-publics target-ns))
         fns (filter #(contains? (meta %) :arglists) all-vars)
         divider "------------------------------------------------"]
     (doseq [f fns]
-      (d/remove-pre-hook! pre-log)
-      (d/remove-post-hook! post-log)
+      (d/remove-pre-hook! f pre-log)
+      (d/remove-post-hook! f post-log)
       (pre-log f divider)
       (post-log f divider))))
 
