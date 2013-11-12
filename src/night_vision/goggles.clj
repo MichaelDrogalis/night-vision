8f40711292e84e125fc45f1c5a8985d86fda1823
Michael Drogalis <mjd3089@rit.edu>
2013-11-12 10:51:39 -0500

    Use 0.5.2 for debug removal.

--- src/night_vision/goggles.clj
+++ src/night_vision/goggles.clj
@@ -2,21 +2,27 @@
   (:require [clojure.pprint :refer [pprint]]
             [dire.core :as d]))
 
+(defn pre-log [f divider]
+  (d/with-pre-hook! f
+    (fn [& args]
+      (pprint divider)
+      (pprint (str "Invoking " f))
+      (pprint (str "Actual arguments are: " args))
+      (pprint divider))))
+
+(defn post-log [f divider]
+  (d/with-post-hook! f
+    (fn [result]
+      (pprint divider)
+      (pprint (str "Returning from " f))
+      (pprint (str "Return value is: " result))
+      (pprint divider))))
+
 (defn introspect-ns! [target-ns]
   (let [all-vars (vals (ns-publics target-ns))
         fns (filter #(contains? (meta %) :arglists) all-vars)
         divider "------------------------------------------------"]
     (doseq [f fns]
-      (d/with-pre-hook! f
-        (fn [& args]
-          (pprint divider)
-          (pprint (str "Invoking " f))
-          (pprint (str "Actual arguments are: " args))
-          (pprint divider)))
-      (d/with-post-hook! f
-        (fn [result]
-          (pprint divider)
-          (pprint (str "Returning from " f))
-          (pprint (str "Return value is: " result))
-          (pprint divider))))))
+      (pre-log f divider)
+      (post-log f divider))))
 
